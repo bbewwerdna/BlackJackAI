1cbbddb54f82ca08e8151372633560f5060460e2
bbewwerdna <30481172+bbewwerdna@users.noreply.github.com>
2018-11-29 17:20:14 -0600

    Started the ai part of the game

---
+++ CardStack.cs
@@ -0,0 +1,145 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class CardStack : MonoBehaviour
+{
+    List<int> cards;
+
+    public bool isGameDeck;
+
+    public bool HasCards
+    {
+        get{ return cards != null && cards.Count > 0; }
+    }
+
+    public event CardEventHandler CardRemoved;
+    public event CardEventHandler CardAdded;
+    public int CardCount
+    {
+        get
+        {
+            if(cards == null)
+            {
+                return 0;
+            }
+            else
+            {
+                return cards.Count;
+            }
+        }
+    }
+    public IEnumerable<int> GetCards()
+    {
+        foreach (int i in cards)
+        {
+            yield return i;
+        }
+    }
+
+    public int Pop()
+    {
+        int temp = cards[0];
+        cards.RemoveAt(0);
+        if(CardRemoved != null)
+        {
+            CardRemoved(this, new CardEventArgs(temp));
+        }
+        return temp;
+    }
+
+    public void Push(int card)
+    {
+        cards.Add(card);
+        if(CardAdded != null)
+        {
+            CardAdded(this, new CardEventArgs(card));
+        }
+    }
+
+    public int HandValue()
+    {
+        int total = 0;
+        int aces = 0;
+        foreach(int card in GetCards())
+        {
+            int cardRank = card % 13;
+            //case card value is from 2 - 9
+            if(cardRank < 8)
+            {
+                cardRank += 2;
+                total = total + cardRank;
+            }
+            //case card value is 10
+            else if(cardRank >=8 && cardRank <12)
+            {
+                cardRank = 10;
+                total = total + cardRank;
+            }
+/*            else if(total <22)
+            {
+                cardRank = 11; //needs to be more complex bc it can be 11 or 1
+                total = total + cardRank;
+            }
+            else
+            {
+                cardRank = 1;
+                total = total + cardRank;
+            }
+ */           
+            else
+            {
+                aces++;
+            }
+        }
+        for(int i =0; i<aces; i++)
+        {
+            if(total +11 < 22)
+            {
+                total = total + 11;
+            }
+            else
+            {
+                total = total + 1;
+            }
+        }
+
+
+        return total;
+    }
+
+    public void CreateDeck()
+    {
+        //int shuffle = 15;
+        for (int i = 0; i < 52; i++)
+        {
+            cards.Add(i);
+        }
+
+        int n = cards.Count;
+        while (n > 1)
+        {
+            n--;
+            int k = Random.Range(0, n + 1);
+            int temp = cards[k];
+            cards[k] = cards[n];
+            cards[n] = temp;
+        }
+        //CreateDeck();
+    }
+
+    public void Reset()
+    {
+        cards.Clear();
+    }
+
+    void Awake () 
+    {
+
+        cards = new List<int>();
+        if (isGameDeck)
+        {
+            CreateDeck();
+        }
+	}
+}
---
+++ GameController.cs
@@ -0,0 +1,238 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameController : MonoBehaviour
+{
+    int dealersFirstCard = -1;
+
+    public CardStack player;
+    public CardStack dealer;
+    public CardStack deck;
+    public CardStack AI;
+    public Button HitButton;
+    public Button StayButton;
+    public Button Deal;
+    public int card;
+    List<int> DealerHand = new List<int>();
+
+    public Button Spot1;
+    
+    public Text winText;
+    public Text sit;
+    //public bool button = true;
+
+    List<CardStack> players = new List<CardStack>();
+    
+
+
+    public void Hit()
+    {
+        player.Push(deck.Pop());
+        if(player.HandValue() > 21)
+        {
+
+            //player bust
+            HitButton.interactable = false;
+            StayButton.interactable = false;
+            //StartCoroutine(DealersTurn());
+            StartCoroutine(AiTurn());
+        }
+    }
+    public void Stick()
+    {
+        HitButton.interactable = false;
+        StayButton.interactable = false;
+        // dealer actions
+        //StartCoroutine(DealersTurn());
+        StartCoroutine(AiTurn());
+    }
+
+    public void DealAgain()
+    {
+        Deal.interactable = false;
+
+        player.GetComponent<CardStackView>().Clear();
+        dealer.GetComponent<CardStackView>().Clear();
+        winText.text = "";
+
+        HitButton.interactable = true;
+        StayButton.interactable = true;
+        dealersFirstCard = -1;
+        Debug.Log("deck count" + deck.CardCount);
+        if(deck.CardCount<15)
+        {
+            deck.GetComponent<CardStackView>().Clear();
+            deck.CreateDeck();
+            StartGame();
+        }
+        else
+        {
+            StartGame();
+
+        }
+        
+
+    }
+
+    /* allows player to sit or stand while the game goes on*/
+    public void Sit()
+    {
+        //removes button from scene
+        Spot1.image.enabled = false;
+        Spot1.interactable = false;
+
+        sit.text = "";
+        //puts player at beginning of players list
+        players.Insert(0, player);
+        //StartGame();
+    }
+
+
+    void Start()
+    {
+        StartGame();
+    }
+    void StartGame()
+    {
+        //players.Add(player);
+        HitButton.interactable = false;
+        StayButton.interactable = false;
+        players.Add(AI);
+        players.Add(dealer);
+        sit.text = "sit";
+        int numPlayers = players.Count;
+        for (int i = 0; i < 2; i++)
+        {
+            for (int a = 0; a < players.Count; a++)
+            {
+                //check to see who is playing and who to deal to
+                if (players[a] == player)
+                {
+                    player.Push(deck.Pop());
+                }
+                else if (players[a] == AI)
+                {
+                    HitAI();
+                }
+                else
+                {
+                    HitDealer();
+                }
+            }
+
+            //player.Push(deck.Pop());
+            //AI.Push(deck.Pop());
+            //HitAI();
+            //HitDealer();
+        }
+        Debug.Log("Dcard1 = " + DealerHand[0]);
+        Debug.Log("Dcard2 = " + DealerHand[1]);
+
+        Debug.Log("dealer card"+dealer.HandValue());
+        if (players.Count == 2)
+        {
+            StartCoroutine(AiTurn());
+        }
+        else
+        {
+            HitButton.interactable = true;
+            StayButton.interactable = true;
+        }
+
+    }
+    void HitDealer()
+    {
+        card = deck.Pop();
+        if (dealersFirstCard < 0)
+        {
+            dealersFirstCard = card;
+        }
+        dealer.Push(card);
+        DealerHand.Add(card%13);
+        if(dealer.CardCount>=2)
+        {
+            CardStackView view = dealer.GetComponent<CardStackView>();
+            view.Toggle(card, true);
+        }
+
+        //Debug.Log("Dcard1 = " + DealerHand[0]);
+        //Debug.Log("Dcard2" + DealerHand[1]);
+        
+    }
+    IEnumerator DealersTurn()
+    {
+        HitButton.interactable = false;
+        StayButton.interactable = false;
+        CardStackView view = dealer.GetComponent<CardStackView>();
+        view.Toggle(dealersFirstCard, true);
+        view.ShowCards();
+        yield return new WaitForSeconds(1f);
+        if (player.HandValue() < 22)
+        {
+            while (dealer.HandValue() < 17)
+            {
+                HitDealer();
+                yield return new WaitForSeconds(1f);
+            }
+        }
+
+        if(player.HandValue() > 21 || dealer.HandValue() > player.HandValue() && dealer.HandValue() <22)
+        {
+            winText.text = "You lose";
+        }
+        else if(dealer.HandValue() > 21 || player.HandValue() <22 && player.HandValue() > dealer.HandValue())
+        {
+            winText.text = "You win";
+        }
+        else
+        {
+            winText.text = "Push";
+        }
+        yield return new WaitForSeconds(1f);
+        Deal.interactable = true;
+    }
+
+    void HitAI()
+    {
+        AI.Push(deck.Pop());
+    }
+    IEnumerator AiTurn()
+    {
+        HitButton.interactable = false;
+        StayButton.interactable = false;
+        if(DealerHand[0] == 2 || DealerHand[0] ==3)
+        {
+            if(AI.HandValue()>12)
+            {
+                //stay
+            }
+            else if(AI.HandValue()>1&& AI.HandValue()<9 || AI.HandValue()==12)
+            {
+                //hit
+            }
+            else
+            {
+                //double
+            }
+        }
+        else if(DealerHand[0] == 4)
+        {
+
+        }
+        else if(DealerHand[0] == 5 || DealerHand[0] == 6)
+        {
+
+        }
+        else if(DealerHand[0] >6 && DealerHand[0]<10)
+        {
+
+        }
+        else
+        {
+
+        }
+        yield return new WaitForSeconds(1.5f);
+    }
+}
